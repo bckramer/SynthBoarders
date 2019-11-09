11be979b74a4c397a9b7cd27048c1f4c3d00ebf9
bckramer <bckramer97@gmail.com>
2019-11-08 18:54:16 -0600

    Content and Source

---
+++ Source/SynthBoarders/SynthBoardersGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SynthBoardersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SYNTHBOARDERS_API ASynthBoardersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
